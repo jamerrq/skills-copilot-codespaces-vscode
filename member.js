4aa0c3f09038d70f8e43f4850c1ff5cfbc296268
JaⰮer ⬢ <jamerrq@gmail.com>
2023-11-21 16:32:15 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+    var skills = ["HTML", "CSS", "JS", "React", "Node"];
+    var person = {
+        name: "John",
+        age: 32,
+        city: "New York"
+    };
+
+    return { skills, person };
+}
